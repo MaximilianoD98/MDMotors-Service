d55bc10222acd2a110b34a9d3a48d39896842e68
MaximilianoD98 <maximilianodurandg@gmail.com>
2023-07-17 20:01:22 -0300

    Update objetos y array

--- js.js
+++ js.js
@@ -3,40 +3,112 @@ let variableCambiante = true;
 
 alert ("Bienvenido a MD-Motors")
 
+function cambioDeAceite(fecha,nombreTitular){
+    this.nombre= "Servicio de cambio de aceite";
+    this.valor= 1000;
+    this.fechaDeRealizacion = fecha;
+    this.nombreDelTitular=nombreTitular;
+  }
+
+  function inspeccionMotor(fecha,nombreTitular){
+    this.nombre= "Servicio de inspección de motor";
+    this.valor= 1700;
+    this.fechaDeRealizacion = fecha;
+    this.nombreDelTitular=nombreTitular;
+  }
+
+  function inspeccionFrenos(fecha,nombreTitular){
+    this.nombre= "Servicio de inspección de frenos";
+    this.valor= 1200;
+    this.fechaDeRealizacion = fecha;
+    this.nombreDelTitular=nombreTitular;
+  }
+
+  function inspeccionSuspension(fecha,nombreTitular){
+    this.nombre= "Servicio de inspección de suspensión";
+    this.valor= 1100;
+    this.fechaDeRealizacion = fecha;
+    this.nombreDelTitular=nombreTitular;
+  }
+
+  function inspeccionTransmision(fecha,nombreTitular){
+    this.nombre= "Servicio de inspección de transmisión";
+    this.valor= 1500;
+    this.fechaDeRealizacion = fecha;
+    this.nombreDelTitular=nombreTitular;
+  }
 
 
 
 function opciones (servicio){
     switch (servicio){
         case 1:
-            servicio = "Cambio de aceite y filtros"
-            valor = 1000
+            fechaAct= prompt("Ingrese la fecha actual")
+            nombreCliente=prompt("Ingrese nombre del cliente")
+            let objetoDeTipoCambioDeAceite = new cambioDeAceite(fechaAct,nombreCliente);
+            let listaDetalleDelServicioAceite=[objetoDeTipoCambioDeAceite.nombre,objetoDeTipoCambioDeAceite.valor,objetoDeTipoCambioDeAceite.fechaDeRealizacion,objetoDeTipoCambioDeAceite.nombreDelTitular]
+            alert(listaDetalleDelServicioAceite)
+            servicio = objetoDeTipoCambioDeAceite.nombre;
+            valor = objetoDeTipoCambioDeAceite.valor;
+            
             alert ("Usted a seleccionado " + servicio + "\nEl monto a abonar será de $" + valor);
             break;
         case 2:
-            servicio = "Inspección de motor"
-            valor = 2000
+            fechaAct= prompt("Ingrese la fecha actual")
+            nombreCliente=prompt("Ingrese nombre del cliente")
+            let objetoDeTipoMotor = new inspeccionMotor(fechaAct,nombreCliente);
+            let listaDetalleDelServicioMotor=[objetoDeTipoMotor.nombre,objetoDeTipoMotor.valor,objetoDeTipoMotor.fechaDeRealizacion,objetoDeTipoMotor.nombreDelTitular]
+            alert(listaDetalleDelServicioMotor)
+            servicio = objetoDeTipoMotor.nombre;
+            valor = objetoDeTipoMotor.valor;
+
             alert ("Usted a seleccionado " + servicio + "\nEl monto a abonar será de $" + valor);
             break;
         case 3:
-            servicio = "Inspección de frenos"
-            valor = 1500
+            fechaAct= prompt("Ingrese la fecha actual")
+            nombreCliente=prompt("Ingrese nombre del cliente")
+            let objetoDeTipoFrenos = new inspeccionFrenos(fechaAct,nombreCliente);
+            let listaDetalleDelServicioFrenos=[objetoDeTipoFrenos.nombre,objetoDeTipoFrenos.valor,objetoDeTipoFrenos.fechaDeRealizacion,objetoDeTipoFrenos.nombreDelTitular]
+            alert(listaDetalleDelServicioFrenos)
+            servicio = objetoDeTipoFrenos.nombre;
+            valor = objetoDeTipoFrenos.valor;
+
             alert ("Usted a seleccionado " + servicio + "\nEl monto a abonar será de $" + valor);
             break;
         case 4:
-            servicio = "Inspección de suspensión"
-            valor = 1300
+            fechaAct= prompt("Ingrese la fecha actual")
+            nombreCliente=prompt("Ingrese nombre del cliente")
+            let objetoDeTipoSuspension = new inspeccionSuspension(fechaAct,nombreCliente);
+            let listaDetalleDelServicioSuspension=[objetoDeTipoSuspension.nombre,objetoDeTipoSuspension.valor,objetoDeTipoSuspension.fechaDeRealizacion,objetoDeTipoSuspension.nombreDelTitular]
+            alert(listaDetalleDelServicioSuspension)
+            servicio = objetoDeTipoSuspension.nombre;
+            valor = objetoDeTipoSuspension.valor;
+
             alert ("Usted a seleccionado " + servicio + "\nEl monto a abonar será de $" + valor);
             break;
         case 5:
-            servicio = "Inspección de transmisión"
-            valor = 2000
+            fechaAct= prompt("Ingrese la fecha actual")
+            nombreCliente=prompt("Ingrese nombre del cliente")
+            let objetoDeTipoTransmision = new inspeccionTransmision(fechaAct,nombreCliente);
+            let listaDetalleDelServicioTransmision=[objetoDeTipoTransmision.nombre,objetoDeTipoTransmision.valor,objetoDeTipoTransmision.fechaDeRealizacion,objetoDeTipoTransmision.nombreDelTitular]
+            alert(listaDetalleDelServicioTransmision)
+            servicio = objetoDeTipoTransmision.nombre;
+            valor = objetoDeTipoTransmision.valor;
+
             alert ("Usted a seleccionado " + servicio + "\nEl monto a abonar será de $" + valor);
             break;
         case 6:
-            servicio = "Inspección de completa"
-            valor = 7000
-            alert ("Usted a seleccionado " + servicio + "\nEl monto a abonar será de $" + valor);
+            fechaAct= prompt("Ingrese la fecha actual")
+            nombreCliente=prompt("Ingrese nombre del cliente")
+            let servicioDeTipoAceite = new cambioDeAceite(fechaAct,nombreCliente);
+            let servicioDeTipoMotor = new inspeccionMotor(fechaAct,nombreCliente);
+            let servicioDeTipoFrenos = new inspeccionFrenos(fechaAct,nombreCliente);
+            let servicioDeTipoSuspension = new inspeccionSuspension(fechaAct,nombreCliente);
+            let servicioDeTipoTransmision = new inspeccionTransmision(fechaAct,nombreCliente);
+            servicio = servicioDeTipoAceite.nombre + ", " + servicioDeTipoMotor.nombre + ", " + servicioDeTipoFrenos.nombre + ", " + servicioDeTipoSuspension.nombre + ", " + servicioDeTipoTransmision.nombre;
+            valor = servicioDeTipoAceite.valor + servicioDeTipoMotor.valor + servicioDeTipoFrenos.valor + servicioDeTipoSuspension.valor + servicioDeTipoTransmision.valor;
+
+            alert ("Usted a seleccionado los servicios: " + servicio + "\nEl monto a abonar será de $" + valor);
             break;
         case 7:
             alert ("Gracias por considerarnos, vuelva pronto.");
